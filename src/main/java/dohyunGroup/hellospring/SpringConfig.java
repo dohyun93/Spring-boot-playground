cf8af1e5fb2ecedec7c4c95ba98bca9c71bcfbd4
dohyun93 <kwondohyun93@naver.com>
2022-03-16 23:48:02 +0900

    lecture 11~17 study 11. Member service development 12. Member service test 13. Component scan 14. Java code 15. Member Web function home addition. 16. Member Web function registration 17. Member Web function search

---
+++ src/main/java/dohyunGroup/hellospring/SpringConfig.java
@@ -0,0 +1,19 @@
+//package dohyunGroup.hellospring;
+//
+//import dohyunGroup.hellospring.repository.MemberRepository;
+//import dohyunGroup.hellospring.repository.MemoryMemberRepository;
+//import dohyunGroup.hellospring.service.MemberService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SpringConfig {
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+//}
---
+++ src/main/java/dohyunGroup/hellospring/controller/MemberController.java
@@ -0,0 +1,50 @@
+package dohyunGroup.hellospring.controller;
+
+import dohyunGroup.hellospring.domain.Member;
+import dohyunGroup.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+    // 스프링 빈 등록 방법
+    // 1. 컴포넌트(Controller, Service, Repository) 스캔과 자동 의존관계 설정(Autowired)
+    // 2. 자바 코드로 직접 스프링 빈 등록.
+
+    // 스프링은 스프링 컨테이너에 스프링 빈을 등록할 때 기본으로 싱글톤으로 등록한다 (유일하게 하나만 등록해서 공유한다.)
+    // 따라서 같은 스프링 빈이면 모두 같은 인스턴스다.
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
